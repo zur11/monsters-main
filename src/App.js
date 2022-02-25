89bab34d541e9978493166567eecc006b5844f44
zur11 <cibka11@gmail.com>
2022-02-25 16:24:37 -0500

    1st commit, sect 3/vid 27 Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super(); //* Calls the constructor Method on Component Class
+
+    this.state = { //* super() gives access to this.state
+      string: 'Hello Villa ilalo'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({string: 'Welcome to the Villa'})}>Change Text
+        </button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
