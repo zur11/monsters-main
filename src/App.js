248292413fbf65d86b8b3852958ba0372b1ba7e0
zur11 <cibka11@gmail.com>
2022-02-28 15:55:58 -0500

    5th> building searchField 1, setState 2nd arg

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super(); 
 
     this.state = { 
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   };
 
@@ -22,6 +23,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={e => {
+            this.setState({searchField: e.target.value},
+              () => console.log(this.state) //* setState's 2nd argument callback f
+            );
+          }} 
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     )
